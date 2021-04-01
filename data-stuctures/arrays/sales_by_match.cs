9214bc9e4bc0fbab2036a2caf76524d53ea9d168
Walter Vinicius <walter.cardoso@socialminer.com>
2021-04-01 20:38:46 -0300

    solved sales by match

---
+++ data-stuctures/arrays/sales_by_match.cs
@@ -0,0 +1,28 @@
+    https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=warmup&isFullScreen=true
+    
+    static int sockMerchant(int n, int[] ar) {
+       var map = new Dictionary<int, int>();
+       
+       foreach(var item in ar)
+       {
+           if (map.ContainsKey(item))
+           {   
+                map[item]++;
+                continue;
+           }
+           
+           map.Add(item, 1);       
+       } 
+       
+       int pairs = 0;
+       
+       foreach(var item in map.Keys)
+       {
+           
+           var socks = map[item];
+          
+           pairs += (int) socks/2;
+       }
+       
+       return pairs;
+    }
